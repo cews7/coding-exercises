b106fb2ae4aad0a18509451c1fcdcc9d68b574a0
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-05-16 12:01:39 -0600

    solve roman numeral converter

---
+++ roman_numeral.rb
@@ -0,0 +1,38 @@
+require 'pry'
+class NumeralConverter
+  def initialize
+    @roman_dictionary = {
+      "I" => 1,
+      "V" => 5,
+      "X" => 10,
+      "L" => 50,
+      "C" => 100,
+      "D" => 500,
+      "M" => 1000 }
+  end
+
+  def roman(input)
+    total = 0
+    input.chars.map.with_index do |char, i|
+      if char == 'V' && input.chars[i - 1] == 'I'
+        total += 3
+      elsif char == 'X' && input.chars[i - 1] == 'I'
+        total += 8
+      elsif char == 'L' && input.chars[i - 1] == 'X'
+        total += 30
+      elsif char == 'C' && input.chars[i - 1] == 'X'
+        total += 80
+      elsif char == 'D' && input.chars[i - 1] == 'C'
+        total += 300
+      elsif char == 'M' && input.chars[i - 1] == 'C'
+        total += 800
+      else
+        total += @roman_dictionary[char]
+      end
+    end
+    print total
+  end
+end
+
+sample_input = "MCMXCIV"
+NumeralConverter.new.roman(sample_input)
