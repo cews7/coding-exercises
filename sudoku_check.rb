b510980bc0bb31e4b74fd2f56033eca869db4bbc
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-06-03 10:12:51 -0600

    working on sudoku validation; solve for rows; working on columns

---
+++ sudoku_check.rb
@@ -0,0 +1,67 @@
+require 'pry'
+class Check
+  attr_reader :sudoku
+  attr_accessor :valid
+  
+  def initialize(sudoku)
+    @sudoku = sudoku
+    @valid  = true
+  end
+
+  def sudoku_steps
+    check_rows
+    if @valid
+      check_columns
+    else
+       print 'false'
+    end
+  end
+
+  def check_columns
+    column_number = 0
+    number_in_column = 0
+    numbers_checked = 0
+    until numbers_checked == 81
+      if column_collector.include?(@sudoku[column_number][number_in_column]) && @sudoku[column_number][number_in_column] != "."
+        @valid = false
+        break
+      else
+        column_collector << @sudoku[column_number][number_in_column]
+      end
+      column_number += 1
+      if column_number == 8
+        column_number = 0
+      end
+      numbers_checked += 1
+    end
+  end
+
+  def check_rows
+    @sudoku.map do |row|
+      row_collector = []
+      row.map do |num|
+        if row_collector.include?(num) && num != "."
+          @valid = false
+          break
+        else
+          row_collector << num
+        end
+      end
+      break if !valid
+    end
+  end
+end
+
+sudoku = [
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+
+Check.new(sudoku).sudoku_steps
